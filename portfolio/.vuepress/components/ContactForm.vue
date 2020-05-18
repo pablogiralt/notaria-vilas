1bd67093855de19da07ea1e86996a502f76a542c
Pablo Giralt <pablogiralt@gmail.com>
2020-05-18 18:16:02 +0200

    testing netlify forms

--- portfolio/.vuepress/components/ContactForm.vue
+++ portfolio/.vuepress/components/ContactForm.vue
@@ -1,38 +1,48 @@
 <template>
   <form
-    name="ask-question"
+    name="contacto"
     method="post"
     data-netlify="true"
     data-netlify-honeypot="bot-field"
+    class="contact-form"
     >
-    <input type="hidden" name="form-name" value="ask-question" />
-    <label v-for="(panelist, index) in panelists" :key="index">
-      <input
-        type="radio"
-        name="panelist"
-        :value="panelist"
-        @input="ev => updatePanelist"
-        :checked="panelist === currentPanelist"
-      />
-      <span>{{ panelist }}</span>
-    </label>
-    ...
-    <button>Submit</button>
+
+    <input type="hidden" name="form-name" value="contacto" />
+
+    <p class="contact-form__field-group">
+        <label for="name">Nombre *</label>
+        <input type="text" name="name" required />
+    </p>
+    <p class="contact-form__field-group">
+        <label for="company">Empresa</label>
+        <input type="text" name="company" />
+    </p>
+    <p class="contact-form__field-group">
+        <label for="phone">Teléfono *</label>
+        <input type="text" name="phone" required />  
+    </p>
+    <p class="contact-form__field-group">
+        <label for="email">Email *</label>
+        <input type="email" name="email" required />
+    </p>
+    <p class="contact-form__field-group">
+        <label for="message">Mensaje *</label>
+        <textarea name="message" required></textarea>
+    </p>
+    <p class="contact-form__field-group">
+        <button type="submit">Enviar Formulario</button>
+    </p>
   </form>
 </template>
+
 <script>
-export default {
-  name: "QAForm",
-  methods: {
-    updatePanelist (ev) {
-      this.currentPanelist = ev.target.value
+    export default {
+    name: "contacto",
+    methods: {
+    },
+    data () {
+        return {
+        }
     }
-  },
-  data () {
-    return {
-      panelists: ['Evan You', 'Chris Fritz'],
-      currentPanelist: 'Evan You'
     }
-  }
-}
 </script>
