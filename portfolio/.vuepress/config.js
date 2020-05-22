b4b1231e2f871da31b5044c5774ff6f24ebbb170
Pablo Giralt <pablogiralt@gmail.com>
2020-05-22 14:27:44 +0200

    dar cera

--- portfolio/.vuepress/config.js
+++ portfolio/.vuepress/config.js
@@ -23,15 +23,29 @@ module.exports = {
     [
       'link', { rel: "icon", href: config.favicon },
     ],
-    [
-      'link', { rel: "stylesheet", href: config.headings_font_url }
-    ],
-    [
-      'link', { rel: "stylesheet", href: config.default_font_url }
-    ],
+    // [
+    //   'link', { rel: "stylesheet", href: config.headings_font_url }
+    // ],
+    // [
+    //   'link', { rel: "stylesheet", href: config.default_font_url }
+    // ],
     [
       'meta', { name: "viewport", content: "width=device-width,initial-scale=1" }
-    ]
+    ],
+    ['script', {}, `
+        WebFontConfig = {
+          google: { families: [ 'Roboto:400,900:latin', 'EB+Garamond:700:latin' ] }
+        };
+        (function() {
+          var wf = document.createElement('script');
+          wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+          wf.type = 'text/javascript';
+          wf.async = 'true';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(wf, s);
+        })();
+    `]
   ],
   markdown: {
     anchor: {
